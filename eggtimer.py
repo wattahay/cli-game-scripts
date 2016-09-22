e4337756c03afca7fb7f391663748b965e0c6735
wattahay <w.taylor.worthington@gmail.com>
2016-09-21 21:55:14 -0400

    update to verbose eggtimer

--- eggtimer.py
+++ eggtimer.py
@@ -1,27 +1,33 @@
-# This file demonstrates an egg counting down and hatching into a monster
-
-# ubuntu: sudo apt-get install python3 colorama
 from colorama import Fore, Back, Style
 from time import sleep
 import os
+import re
 
 sub = 8329
-egg = (chr(11052) + chr(sub))
 brokegg = (chr(65857) + chr(65856))
 monster = Fore.RED + Style.NORMAL + chr(9568) + chr(9571) + Style.RESET_ALL
 
+reggex = re.compile('\u2B2C.')
+space = ''
 
-while(True):
+while(space != monster):
 	os.system('clear')
 	if (sub >= 8321):
-		print(chr(11052) + chr(sub) + '\n\n')
+		egg = (chr(11052) + chr(sub))
+		space = egg
 		sub -= 1
 	elif (sub < 8321) & (sub >= 8320):
-		print(brokegg + '\n\n')
+		space = brokegg
 		sub -= 1
 	elif (sub < 8320):
-		print(monster + '\n\n')
+		space = monster
 		sub = 8329
-		sleep(2)
+	print('\n'*10)
+	if (re.match(reggex, space)):
+		print('\r' + ' '*19 + space + '    ...It\'s an egg.' + '\n'*10)
+	elif (space == brokegg):
+		print('\r' + ' '*19 + space + '       Something is hatching!\n' + '\n'*10)
+	else:
+		print('\r' + ' '*19 + space + '       It\'s a Monster!!' + '\n'*10)
 	sleep(4)
 	
