eab83a5a635cf755117a239dcb8c858976f34281
wattahay <w.taylor.worthington@gmail.com>
2016-09-22 00:11:35 -0400

    Fix Centering Issues

--- pybeast.py
+++ pybeast.py
@@ -1,4 +1,3 @@
-from colorama import Fore, Back, Style
 from random import randint
 import sys
 import os
@@ -19,8 +18,8 @@ screen_cols = int(ttyCols / 2)#####################-- global settings --#####
 
 ##################################-- tty dynamic board size settings 
 
-max_play_rows = 35	# alter max game height here
-max_play_cols = 35 	# alter max game width here
+max_play_rows = 99	# alter max game height here
+max_play_cols = 99 	# alter max game width here
 max_beast_cnt = 10	# alter max beasts here
 
 ##################################-- starting game statistics
@@ -29,7 +28,7 @@ beast_cnt = 4 		# starting beasts
 lives = 5		# starting lives
 
 squish_boxes = False	# boxes shuv into nothing against blocks
-wall_hit_death = False	# player dies against walls
+block_hit_death = False	# player dies when hitting blocks
 
 ##################################-- game scoring balance
 
@@ -45,23 +44,13 @@ beast_speed = 1.5	# seconds between enemy moves
 monster_speed = 1.5	# seconds between enemy moves
 egg_speed = 4		# seconds between countdowns
 
-lcd_time = .07		# frame-rate of entire game (CODISTADIT)
+min_incubate = 5	# minimum time before eggs start counting down
+max_incubate = 30	# maximum time before eggs start counting down
 
-###################################################################################
-##################################-- formatted character constants--###############
-###################################################################################
+lcd_time = .06
+		# frame-rate of entire game (CODISTADIT)
 
-eggsub = 8329			# unicode key for subscript 9 (8328 = 8, and so on)
-egg2nd = 32			# unicode key for a space character 
-REGGX = re.compile('\u2B2C.') 	# use re.match(REGGX, char) to see if a piece is an egg
-
-EGG = 		Fore.WHITE + 	Style.BRIGHT + 	chr(11052) + chr(egg2nd) + Style.RESET_ALL
-BAKGRD = 					'  ' 
-BLOCK =	Back.YELLOW + 	Style.NORMAL + 	'  ' + 			Style.RESET_ALL
-BOX = 		Fore.GREEN + 			chr(9618) + chr(9618) + Style.RESET_ALL
-BEAST = 	Fore.RED + 	Style.NORMAL + 	chr(9500) + chr(9508) + Style.RESET_ALL
-MONSTER = 	Fore.RED + 	Style.NORMAL + 	chr(9568) + chr(9571) + Style.RESET_ALL
-PLAYER = 	Fore.BLUE + 			chr(9664) + chr(9654) + Style.RESET_ALL
+####################################-- pawn move constants
 
 MOVES = {
 'U': {	'di':'U', 	'ra':-1, 	'ca':0, 	'mm':-1},
@@ -85,49 +74,61 @@ MVDR = 'DR'
 
 DIR_LIS = (MVU, MVD, MVL, MVR, MVUL, MVUR, MVDL, MVDR)
 
-####################################################################################################
-move = '' ###################################################-- frame-scale globals-- ##############
-####################################################################################################
+keypress = '' 
 
-###################################-- Piece Dictionaries
+##################################-- formatted character constants
 
-#beast_steps = int(beast_speed / lcd_time) - 1		# number of frames in between beast moves
-#monster_steps = int(monster_speed / lcd_time) - 1	# number of frames in between monser moves
-#egg_steps = int(egg_speed / lcd_time) - 1		# number of frames in between egg stages
+eggsub = 8329			# unicode key for subscript 9 (8328 = 8, and so on)
+egg2nd = 32			# unicode key for a space character 
+REGGX = re.compile('\u2B2C.') 	# use re.match(REGGX, char) to see if a piece is an egg
 
+# (ANSI styles)	 FG   + 	BG   + 		Style +		Characters +			Reset
+BAKGRD = 							'  ' 
+BLOCK =		       		'\033[43m' +         		'  ' + 				'\033[0m'
+BOX = 		'\033[32m' +					chr(9618) + chr(9618) +		'\033[0m'
+EGG = 		'\033[37m' +					chr(11052) + chr(egg2nd) + 	'\033[0m'
+BEAST = 	'\033[31m' +					chr(9500) + chr(9508) +		'\033[0m'
+MONSTER = 	'\033[31m' +					chr(9568) + chr(9571) +		'\033[0m'
+PLAYER = 	'\033[34m' +					chr(9664) + chr(9654) +		'\033[0m'
+# http://wiki.bash-hackers.org/scripting/terminalcodes
 
-beasts = [{'chr': BEAST, 'steps':((int(beast_speed / lcd_time)) - 1), 'frame': 0}] 
-# 'ro' = row of each item
-# 'co' = col of each item
-# 'fow' = potential move
-# 'fol' = potential move
-# 'seed' = the interval at which each beast moves
-# 'mv' = direction of travel
-monsters = [{'chr': MONSTER, 'steps':((int(beast_speed / lcd_time)) - 1), 'frame': 0}]
-# 'ro' = row of each item
-# 'co' = col of each item
-# 'fow' = potential move
-# 'fol' = potential move
-# 'seed' = the interval at which each monster moves 
-# 'mv' = direction of travel
-eggs = [{'chr': EGG, 'steps':((int(beast_speed / lcd_time)) - 1), 'frame': 0}]
-# 'ro' = row of each item
-# 'co' = col of each item
-# 'fow' = potential move
-# 'fol' = potential move
-# 'seed' = the interval at which each egg hatches
-# 'sub' = the unicode number of the subscript countdown
-# 'tmr' = the countdown until each egg hatches
-# 'mv' = direction of travel
-push = False
-tug = False
-player = [{'chr':PLAYER}, {'mv':'', 'push':False, 'tug': False, 'ro':0, 'co':0}]
-# 'ro' = row
-# 'co' = col
-# 'fow' = potential move
-# 'fol' = potential move
-# 'mv' = direction of travel
+###################################-- Pawn Classes (Dictionaries)
+##################################-- Pawn Lists 
 
+# pawn lists dynamically grows and shrink with 
+
+beasts = [{
+	'chr': BEAST, 
+	'frame':0,
+	'frames': ((int(beast_speed / lcd_time)) - 1)
+	}]
+
+monsters = [{
+	'chr': MONSTER, 
+	'frame':0,
+	'frames': ((int(beast_speed / lcd_time)) - 1)
+	}]
+
+eggs = [{
+	'chr': EGG, 
+	'frame':0,
+	'frames': ((int(beast_speed / lcd_time)) - 1)
+	}]
+
+# 'sub'		updated digital unicode reference to subscript character
+# 'wait'	randomized wait time before hatching countdown
+player = [{
+		'chr': PLAYER, 
+	}]
+	
+# 'push'
+# 'tug'
+# 'ro'		row
+# 'co'		col
+# 'fow'		potential move
+# 'fol'		potential move
+# 'mv'		direction of travel
+# 'stag'	individual frame
 ################################################################################################
 ####################################################################-- level 1 setup --#########
 ################################################################################################
@@ -157,20 +158,19 @@ left_margin = 0
 top_margin = 0
 max_play_rows = int(max_play_rows / 3) * int(3)
 max_play_cols = int(max_play_cols / 3) * int(3)
-min_play_rows = round((play_rows + 6) / 3)
-min_play_cols = round((play_cols + 6) / 3)
+min_play_rows = 9
+min_play_cols = 9
 board_rows = play_rows + 2
 board_cols = play_cols + 2
 debug = False
 
 def plan_the_board(): #{
 
-	global top_margin, left_margin, board_rows, board_cols, play_rows, play_cols, min_play_rows, min_play_cols, max_play_rows, max_play_cols, stat_rows, debug_rows, ttyCols, screen_rows, screen_cols
+	global top_margin, left_margin, board_rows, board_cols, play_rows, play_cols, min_play_rows, min_play_cols, max_play_rows, max_play_cols, stat_rows, ttyCols, screen_rows, screen_cols
 
-	set_margin = 2
-	fill_margin = 4 # This prevents fill boards from completely removing margins
+	padding = 2 # the blank border around a board
+	fill_margin = 2 + padding # This prevents fill boards from completely removing margins
 	stat_rows = 2
-	debug_rows = 5
 	used_rows = 0
 	used_cols = 0
 	 
@@ -185,31 +185,28 @@ def plan_the_board(): #{
 			if (play_rows > max_play_rows):
 				play_rows = max_play_rows
 			board_rows = play_rows + 2
-			used_rows = play_rows + fill_margin + stat_rows
+			used_rows = board_rows + stat_rows
 			top_margin = int((screen_rows - used_rows) / 2)
 		else: # if the board is either small or huge 
-			play_rows = int((screen_rows - set_margin - stat_rows) / 3) * int(3)
+			play_rows = int((screen_rows - padding - stat_rows) / 3) * int(3)
 			if (play_rows > max_play_rows):
 				play_rows = max_play_rows
 			board_rows = play_rows + 2
-			used_rows = board_rows + set_margin + stat_rows
+			used_rows = board_rows + stat_rows
 			top_margin = int((screen_rows - used_rows) / 2)
 
-
 		if ((screen_cols <= (max_play_cols + fill_margin)) & (screen_cols > (min_play_cols + fill_margin))):
-			play_cols = int((screen_cols - fill_margin) / 3) * int(3)
+			play_cols = (int((screen_cols - fill_margin) / 3) * (int(3)))
 			if (play_cols > max_play_cols):
 				play_cols = max_play_cols
 			board_cols = play_cols + 2
-			used_cols = play_cols + fill_margin
-			left_margin = int((ttyCols - used_cols*2) / 2)
+			left_margin = int((ttyCols - (board_cols * 2)) / 2)
 		else: # if the board is either small or huge 
-			play_cols = int((screen_cols - set_margin) / 3) * int(3)
+			play_cols = int((screen_cols - padding) / 3) * int(3)
 			if (play_cols > max_play_cols):
 				play_cols = max_play_cols
 			board_cols = play_rows + 2
-			used_cols = board_cols + set_margin
-			left_margin = int((ttyCols - used_cols*2) / 2)
+			left_margin = int((ttyCols - board_cols * 2) / 2)
 
 
 	#}
@@ -231,55 +228,56 @@ def build_the_board(): #{
 			if(rowi == 0) | (rowi == (board_rows - 1)) | (coli == 0) | (coli == (board_cols - 1)):
 				board[rowi][coli] = BLOCK
 
-	
-
 #}
+
+
+
+
 
 
 def print_board(): #{
 
 	global ttyCols, top_margin, left_margin, score, lives, level, board_rows, board_cols
+
+
+	# Hide the cursor
+	os.system('tput civis')
 	
 	bottom_margin = top_margin - 3
 
-	#
-	#
-	#
-	#
-	#
-	#
-	#
-	#
-	###########-- Print Top Margin	
+	#     |
+	#     V
+	#     |
+	#     V
+	#     |
+	#     V
+	#     |
+	#     V
+	####-- Print Top Margin	
 	print('\n'*top_margin)
 	
 					#########################################
 					#					#
-					#	H				#
-	##########-- Print Board	#		H	<>		#
+	####-- Print Left Margin ->->-> #	H				#
+	####-- Print Board		#		H	<>		#
 	for rowi in range(board_rows):	#					#
 		print('\r' + ' '*left_margin + ''.join(board[rowi]))		#
 					#				H	#
 					#					#
 					#	H				#
 					#########################################
-
-	############-- Print Game Stats
+	####-- Print Game Stats ------>  Score: 200  Lives: 4  Level: 3
 	print('\r' + ' '*left_margin + '   SCORE: ' + str(score) + '   LIVES: ' + str(lives) + '    LEVEL: ' + str(level)) 
 
-	############-- Print Debug Stats
+	####-- Print Debug Stats
 	if(debug):
-		print('\n\r' + ' '*left_margin + 'Player: ' + str(player) + '\n\rBeasts: ' + str(beasts) + '\n\rBSeeds: '  + '\n\rBeast Stepper: ' + str(beasts[0][stpr]) ) 
+		print(
+			'\n\r Player: ' + str(player) +
+			'\n\rBeasts: ' + str(beasts) +
+			'\n\rBeast Stepper: ' + str(beasts[0]['frame'])
+		 ) 
 
-	############-- Move Cursor Down
-	print('\n'*bottom_margin + '\r')	
-	#
-	#
-	#
-	#
-	#
-	#
-	#
+
 	
 
 
@@ -307,7 +305,6 @@ def place_blocks(): #{
 
 	global play_rows, play_cols, BLOCK
 
-
 	row_ranges = []
 	col_ranges = []
 
@@ -320,9 +317,17 @@ def place_blocks(): #{
 		step = 9 
 		rowi = 0
 		coli = 0
+		while(step >= 0):
+			row_range_index = randint(0, step)
+			rowi = randint(row_ranges[row_range_index][0], row_ranges[row_range_index][1])
+			col_range_index = randint(0, step)
+			coli = randint(col_ranges[col_range_index][0], col_ranges[col_range_index][1])
+			board[rowi][coli] = BLOCK
+			step -= 1
+			del col_ranges[col_range_index]
+			del row_ranges[row_range_index]
 	else:
 		block_cnt = round(play_rows * play_cols / 90 )
-		
 		place_randomly(BLOCK, block_cnt)
 				
 #}
@@ -335,14 +340,10 @@ def place_boxes():
 		box_cnt = randint(210,225)
 	else:
 		prerange = int(play_rows * play_cols / 4)
-		
 		lower = prerange + (int(prerange / 20))
-		
 		upper = prerange + (int(prerange / 20 * 2))
-		
 		box_cnt = randint(lower,upper)
-	        
-		place_randomly(BOX, box_cnt)	
+	place_randomly(BOX, box_cnt)
 			
 
 	
@@ -361,10 +362,11 @@ def place_pawns(pieces, count):
 		col = randint(1, (board_cols - 1))
 		if(board[row][col] == BAKGRD):
 			board[row][col] = pawn 
-			pieces.append({'ro':row, 'co':col, 'tug':False})
+			pieces.append({'ro':row, 'co':col, 'tug':False, 'stag':0})
+			if (pawn != PLAYER):
+				pieces[step]['stag'] = randint(1, (pieces[0]['frames']))
 			step += 1
-			if(pawn == BEAST) | (pawn == MONSTER) | (pawn == EGG):
-				pieces[step]['seed'] = randint(0, (pieces[0]['steps']))
+
 	return pieces
 	
 
@@ -382,7 +384,6 @@ def move_pawns(godir, pieces, index):
 	global BAKGRD, BOX, MVU, MVL, MVR, MVD, MVUL, MVUR, MVDL, MVDR
 	
 	char = pieces[0]['chr']
-
 	row = pieces[index]['ro']
 	col = pieces[index]['co']
 	fow = row
@@ -423,24 +424,23 @@ def move_pawns(godir, pieces, index):
 		fol = col + 1
 	if (board[fow][fol] == BAKGRD):
 		board[fow][fol] = char 
-		if (pieces[1]['tug'] == True) & (board[rtug][ctug] == BOX):
+		if (pieces[index]['tug']) & (board[rtug][ctug] == BOX):
 			board[rtug][ctug] = BAKGRD
 			board[row][col] = BOX
 		else:
 			board[row][col] = BAKGRD
 		pieces[index]['ro'] = fow
 		pieces[index]['co'] = fol
-	
 
 
 
 def move_dist_enemies(pieces): #{
 	# pick one of the beasts to move
-	for bi in range(1, len(pieces)):
-		if pieces[bi]['seed'] == pieces[0]['frame']:
+	for i in range(1, len(pieces)):
+		if pieces[i]['stag'] == pieces[0]['frame']:
 			dirindex = randint(0, len(DIR_LIS) - 1) # randomly pick a direction to go
 			direction = DIR_LIS[dirindex]
-			move_pawns(direction, pieces, bi)
+			move_pawns(direction, pieces, i)
 #}
 
 
@@ -466,14 +466,13 @@ def move_player(mv):
 
 	global player, MVU, MVL, MVD, MVR
 
-	if (mv == 'w'):
+	if (mv == 'o'):
 		move_pawns(MVU, player, 1)
-	elif (mv == 'a'):
+	elif (mv == 'k'):
 		move_pawns(MVL, player, 1)
-	elif (mv == 's'):
+	elif (mv == 'l'):
 		move_pawns(MVD, player, 1)
-	elif (mv == 'd'): 
-		move_pawns(MVR, player, 1)
+	elif (mv == ';'): move_pawns(MVR, player, 1)
 
 
 
@@ -505,7 +504,7 @@ def intro():
 			' '*(introleft - 7)
 			+ 'Enter \'t\' to play a terminal-size board.\n\n\r' 
 			+ ' '*(introleft - 7)
-			+ 'Enter \'c\' to play a class sized board. . . '
+			+ 'Enter \'c\' to play a classic sized board. . . '
 			+ '\n\n\r' 
 			+ ' '*int(ttyCols/2)
 			)
@@ -547,24 +546,29 @@ player = place_pawns(player, 1 )
 ############################-- the main loop that waits for key presses
 
 def takeInput():
-	global debug, move, player
+	global debug, keypress, player
 	while(True):
-		move = inkey(1)
-		if move == 'q':
+		keypress = inkey(1)
+		if keypress == 'q':
 			os.system('clear')
 			exit()
-		elif move == 'b':
+		elif keypress == 'b':
 			if debug == True:
 				debug = False
 			else:
 				debug = True
-		elif move == ' ':
-			while (move == ' '):
-				move = inkey(1)
+		elif keypress == ' ':
+			while (keypress == ' '):
 				player[1]['tug'] = True
-				sleep(lcd_time)
-		move_player(move)
-		sleep(lcd_time)
+				keypress = inkey(1)
+				if (keypress != ' '):
+					move_player(keypress)
+					player[1]['tug'] = True
+					sleep(lcd_time - .02)
+		else:
+#			player[1]['tug'] = False
+			move_player(keypress)
+			sleep(lcd_time - .02)
 
 
 # start the thread that runs the input loop
@@ -577,19 +581,19 @@ t.start()
 ############################-- the main game clock and print loop
 
 while(True):
-	global move
 	os.system('clear')
-	if move == 'q':
+	if keypress == 'q':
+		os.system('reset')
 		exit()
 	move_dist_enemies(beasts)
 	print_board()
-	if move == 'p':
+	if keypress == 'p':
 		input()
-	if beasts[0]['frame'] == monsters[0]['steps']:
+	if beasts[0]['frame'] == beasts[0]['frames']:
 		beasts[0]['frame'] = 0
 	else:
 		beasts[0]['frame'] += 1
-	if monsters[0]['frame'] == monsters[0]['steps']:
+	if monsters[0]['frame'] == monsters[0]['frames']:
 		monsters[0]['frame'] = 0
 	else:
 		monsters[0]['frame'] += 1
