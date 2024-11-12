b6f0a66cebc2076325615a2b2fa8abf061aaad41
wattahay <w.taylor.worthington@gmail.com>
2024-11-12 08:45:30 -0500

    Update beast.py

--- beast.py
+++ beast.py
@@ -89,9 +89,9 @@ PRIORITY_ODDS = [
 dir_keys = 0 #   0=wasd     1=arrows     2=hjkl
 
 KYBD = [ # Get individual key codes using: python3 getkeycodes.py (included in the git repo)
-		{"title":"w,a,s,d", "K_UP":119, "K_DOWN":115, "K_RIGHT":100, "K_LEFT":97, "PK_UP":87, "PK_DOWN":83, "PK_RIGHT":68, "PK_LEFT":65},
-		{"title":"arrows", "K_UP":259, "K_DOWN":258, "K_RIGHT":261, "K_LEFT":260, "PK_UP":337, "PK_DOWN":336, "PK_RIGHT":402, "PK_LEFT":393},
-		{"title":"h,j,k,l", "K_UP":107, "K_DOWN":106, "K_RIGHT":108, "K_LEFT":104, "PK_UP":75, "PK_DOWN":74, "PK_RIGHT":76, "PK_LEFT":72}
+		{"title":"w,a,s,d", "K_UP":119, "K_DOWN":115, "K_RIGHT":100, "K_LEFT":97,  "PK_UP":87,  "PK_DOWN":83,  "PK_RIGHT":68,  "PK_LEFT":65},
+		{"title":"arrows",  "K_UP":259, "K_DOWN":258, "K_RIGHT":261, "K_LEFT":260, "PK_UP":337, "PK_DOWN":336, "PK_RIGHT":402, "PK_LEFT":393},
+		{"title":"h,j,k,l", "K_UP":107, "K_DOWN":106, "K_RIGHT":108, "K_LEFT":104, "PK_UP":75,  "PK_DOWN":74,  "PK_RIGHT":76,  "PK_LEFT":72}
 	]
 ################################################################################################
 ###########################################################-- More Variables --#################
